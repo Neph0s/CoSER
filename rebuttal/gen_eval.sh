3b15972856aeb2968d692fcf360c046360e36526
Neph0s <xtwang21@m.fudan.edu.cn>
2025-03-26 15:35:10 +0800

    add rebuttal codes

---
+++ rebuttal/gen_eval.sh
@@ -0,0 +1,2 @@
+python rebuttal/eval.py --test_file data/test/id_set.json --actor_m
+odel gpt-4o --judge_model gpt-4o
---
+++ rebuttal/rename.py
@@ -0,0 +1,39 @@
+
+''' 
+之前的数据名格式为: 
+exp_name = f'1221-continue_from=0-200case-env_model=gpt-4o-nsp=llama3-1210-8epc'
+simulation_path = f'./exp/results_old/{exp_name}-test_circumstance_id_{actor_model}.json'
+evaluation_path = f'./exp/results_old/{exp_name}-test_circumstance_id_{actor_model}_eval.json'
+'''
+
+actor_models = ['groundtruth', 'llama3-1210-8epc', 'llama3p1-8b-0119', 'abab7-preview-chat', 'llama3p1-8b-instruct', 'gpt3.5', 'gpt-4o', 'claude35sonnet']
+import json 
+
+for actor_model in actor_models:
+    if actor_model == 'groundtruth': 
+        continue 
+
+    exp_name = f'1221-continue_from=0-200case-env_model=gpt-4o-nsp=llama3-1210-8epc'
+    simulation_path = f'./exp/results_old/{exp_name}-test_circumstance_id_{actor_model}.json'
+    evaluation_path = f'./exp/results_old/{exp_name}-test_circumstance_id_{actor_model}_eval.json'
+
+    # 读取simulation_path
+    with open(simulation_path, 'r') as f:
+        simulation_data = json.load(f)
+
+    # 读取evaluation_path
+    with open(evaluation_path, 'r') as f:
+        evaluation_data = json.load(f)
+
+    new_simulation_path = f'./exp/simulation/id_set_{actor_model}.json'
+    new_evaluation_path = f'./exp/evaluation/id_set_{actor_model}.json'
+
+    print(f'Successfully loaded data from {simulation_path} and {evaluation_path}')
+
+    # 写入新的simulation_path
+    with open(new_simulation_path, 'w') as f:
+        json.dump(simulation_data, f, ensure_ascii=False)
+
+    # 写入新的evaluation_path
+    with open(new_evaluation_path, 'w') as f:
+        json.dump(evaluation_data, f, ensure_ascii=False)
